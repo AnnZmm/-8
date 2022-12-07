e04593f708a5b05f6cc82030f4f275659fcd3662
AnnZmm <120049241+AnnZmm@users.noreply.github.com>
2022-12-07 22:34:30 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main(String []args)
+    {
+        List list = new List();
+        list.addString("Hello");
+        list.addString("Hello");
+        list.addString("Hi");
+        list.addString("Hello my friend");
+        list.addString("Good morning");
+        list.addString("Good evening");
+
+        list.removeString("Hi");
+
+        System.out.println(list.list);
+        System.out.println(list.reverseStrings());
+        System.out.println(list.searchSubstring("morning"));
+        System.out.println(list.length());
+        System.out.println(list.searchStrings("Hello"));
+        list.numberOfSymbols();
+    }
+}
